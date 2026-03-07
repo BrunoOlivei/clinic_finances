dc555b2374eea63a6655cd9607eebcb846de3b55
Bruno Oliveira <brunooliveiradiblab@gmail.com>
2026-03-07 17:19:57 -0300

    data ingest bronze

---
+++ src/schemas/__init__.py
@@ -0,0 +1,7 @@
+from src.schemas.atendimentos_sao_lucas_bronze import AtendimentoSaoLucasBronzeSchema
+from src.schemas.atendimentos_sao_lucas_silver import AtendimentoSaoLucasSilverSchema
+
+__all__ = [
+    "AtendimentoSaoLucasBronzeSchema",
+    "AtendimentoSaoLucasSilverSchema",
+]
---
+++ src/services/__init__.py
@@ -0,0 +1,7 @@
+from src.services.atendimentos_sao_lucas_bronze import AtendimentosSaoLucasBronze
+from src.services.atendimentos_sao_lucas_silver import AtendimentosSaoLucasSilver
+
+__all__ = [
+    "AtendimentosSaoLucasBronze",
+    "AtendimentosSaoLucasSilver"
+]
