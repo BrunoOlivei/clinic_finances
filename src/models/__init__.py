e05a358fe60f96b0a81e2fd6da79b6a9f1f2b1d7
Bruno Oliveira <brunooliveiradiblab@gmail.com>
2026-03-07 17:59:15 -0300

    add log model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,7 +1,9 @@
 from src.models.atendimentos_sao_lucas_bronze import AtendimentosSaoLucasBronze
 from src.models.atendimentos_sao_lucas_silver import AtendimentosSaoLucasSilver
+from src.models.log import Log
 
 __all__ = [
     "AtendimentosSaoLucasBronze",
     "AtendimentosSaoLucasSilver",
+    "Log",
 ]
