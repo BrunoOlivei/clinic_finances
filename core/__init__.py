4c1f7e3ea89390fea8d43bad31a55256a8378842
Bruno Oliveira <brunooliveiradiblab@gmail.com>
2026-03-06 22:55:30 -0300

    delete files

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from core.settings import settings
-from core.database import db
-from core.logger import logger
-
-__all__ = ["settings", "db", "logger"]
--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-from models.base import Base
-from models.insurance_claim import InsuranceClaim
-from models.log import Log
-from models.patient import Patient
-from models.procedures import Procedure
-from models.invoice import Invoice
-
-__all__ = ["Base", "InsuranceClaim", "Log", "Patient", "Procedure", "Invoice"]
