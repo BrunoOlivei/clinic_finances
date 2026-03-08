3c91927fa4fbf0eea9d1c33c1c66342102363d70
Bruno Oliveira <brunooliveiradiblab@gmail.com>
2026-03-08 16:57:25 -0300

    create tb_recibos_sao_lucas table at bronze schema

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,11 +3,13 @@ from src.models.atendimentos_sao_lucas_silver import AtendimentosSaoLucasSilver
 from src.models.log import Log
 from src.models.solicitacoes_sao_lucas_bronze import SolicitacoesSaoLucasBronze
 from src.models.solicitacoes_sao_lucas_silver import SolicitacoesSaoLucasSilver
+from src.models.recibos_sao_lucas_bronze import RecibosSaoLucasBronze
 
 __all__ = [
     "AtendimentosSaoLucasBronze",
     "AtendimentosSaoLucasSilver",
     "SolicitacoesSaoLucasBronze",
     "SolicitacoesSaoLucasSilver",
+    "RecibosSaoLucasBronze",
     "Log",
 ]
