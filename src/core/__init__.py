1e14043eb906f2fa16a3a1ad54b5114fd17d0250
Bruno Oliveira <brunooliveiradiblab@gmail.com>
2026-03-08 15:27:38 -0300

    add method to format type data for ingest on silver tables

--- src/core/__init__.py
+++ src/core/__init__.py
@@ -1,9 +1,19 @@
-from src.core.settings import settings
+from src.core.base import Base, BronzeBase, GoldBase, SilverBase
 from src.core.database import db
 from src.core.logger import logger
-from src.core.base import Base, BronzeBase, SilverBase, GoldBase
 from src.core.read_file import ReadFile
-from src.core.utils import set_default_dt_base
+from src.core.settings import settings
+from src.core.utils import format_type_data, set_default_dt_base
 
-
-__all__ = ["settings", "db", "logger", "Base", "BronzeBase", "SilverBase", "GoldBase", "ReadFile", "set_default_dt_base"]
+__all__ = [
+    "settings",
+    "db",
+    "logger",
+    "Base",
+    "BronzeBase",
+    "SilverBase",
+    "GoldBase",
+    "ReadFile",
+    "set_default_dt_base",
+    "format_type_data",
+]
--- src/core/utils.py
+++ src/core/utils.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+from typing import Dict
+
+import pandas as pd
 
 from src.core.logger import logger
 
@@ -25,3 +28,48 @@ def set_default_dt_base(dt_base: int | None) -> int:
     else:
         logger.info(f"dt_base definido: {dt_base}")
         return dt_base
+
+
+def format_type_data(df: pd.DataFrame, schema: Dict[str, Dict[str, str]]) -> pd.DataFrame:
+    """
+    Format the data types of the DataFrame columns according to the defined schema.
+
+    Args:
+        df (pd.DataFrame): The DataFrame to be formatted.
+        schema (Dict[str, Dict[str, str]]): 
+
+    Returns:
+        pd.DataFrame: The DataFrame with formatted data types.
+    """
+    for column, properties in schema.items():
+        if column not in df.columns:
+            continue
+        if properties["type"] == "date":
+            df[column] = pd.to_datetime(
+                df[column],
+                format=properties["format"],
+                errors="coerce",
+                dayfirst=True,
+            ).dt.date
+        elif properties["type"] == "time":
+            df[column] = pd.to_datetime(
+                df[column], format=properties["format"], errors="coerce"
+            ).dt.time
+        elif properties["type"] == "datetime":
+            df[column] = pd.to_datetime(
+                df[column], format=properties["format"], errors="coerce", dayfirst=True
+            )
+        elif properties["type"] == "integer":
+            df[column] = pd.to_numeric(df[column], errors="coerce").astype("Int64")
+        elif properties["type"] == "string":
+            df[column] = df[column].str.strip().astype(str)
+        elif properties["type"] == "boolean":
+            df[column] = df[column].apply(
+                lambda x: True if x.lower() in ["true", "1", "yes", "sim"] else False
+            )
+        else:
+            raise ValueError(
+                "Error formating type of columns data"
+            )
+            
+    return df
